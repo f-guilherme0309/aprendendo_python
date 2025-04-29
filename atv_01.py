4bcb393beb039306d25d9cb26e65e102cc157d69
Fernando Guilherme mota de amorim <fergui18mota@gmail.com>
2025-04-29 11:20:53 -0400

    Add files via upload

---
+++ atv_01.py
@@ -0,0 +1,3 @@
+numero1 = input("digite numero 1: ")
+numero2 = input ("digite numero 2: ")
+print(f"numero 1: {numero1} numero 2: {numero2}")
---
+++ atv_02.py
@@ -0,0 +1,6 @@
+numero = int(input("digite um numero:"))
+
+if numero % 2 >=1 :
+    print("impar")
+else:
+     print("par")
